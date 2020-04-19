be294eb79672c51aaef5cfdd2a252df2afb96b0a
Meshal alotaibi <meshal.saus@gmail.com>
2020-04-19 13:13:03 +0300

    adding

---
+++ Password Generator.js
@@ -0,0 +1,40 @@
+//generate random password
+function generate() {
+
+    //set password length/complexity
+    var complexity = document.getElementById("slider").value;
+
+    //possible password values
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    var password = "";
+
+    //create for loop to choose password characters .
+    for (var i = 0; i < complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * (values.length - 1)));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+    //add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+}
+
+//set default length display of 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+//function to set length based on slider position
+document.getElementById("slider").oninput = function () {
+
+    if (document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+
+}
+
+
+
+
+
